5721597de4125a47058f321a65beb603aa9ac449
SunkissedQueen <baxtercm@outlook.com>
2023-09-05 16:35:29 -0700

    javascript high order functions notes to include truthy, falsy, type coercion, and for loop example for .filter()

--- javascript-high-order-functions.js
+++ javascript-high-order-functions.js
@@ -106,10 +106,61 @@
     //   '4. Black Panther 💥'
     // ]
 
-// .filter()
-// .filter(): allows us to create a subset or new array with the values that satisfy a given condition
 
 // prompt: create a function that takes in an array and returns an array with values that do not include "man" in their name.
+
+// test variable
+// const superHero = [ "Superman", "Batman", "Wonder Woman", "Captain Marvel", "Black Panther" ]
+
+// pseudocode:
+  // name of function: noMan
+  // input: array with the names of superheroes as values
+  // output: array with values that do not include "man" in their name
+  // process:
+    // create a variable to store an empty array (manList)
+    // iteration to perform an action to each value, for loop
+    // conditional statements that use the bang operator. Since .includes method would give values that are true to the condition of "man" included in the name, use the bang operator to give values that are false to the condition 
+    // push method to add values to the end of the empty array
+    // return the new array filled with values that satisfy the given condition
+
+// // function expression
+// const noMan = (array) => {
+//   // empty array to store values
+//   let manList = []
+//   // iterate across each value using the number created from for loop
+//   for(let i = 0; i < array.length; i++) {
+//     // bang operator used to find values that are false to the inclusion of "man"
+//     if(! array[i].includes("man") ) {
+//       // push value to the end of the empty array
+//       manList.push(array[i])
+//     } 
+//     // return outside of for loop to allow all values to be evaluated
+//   } return manList
+// }
+
+// // function call
+// console.log(noMan(superHero))
+// output: [ 'Captain Marvel', 'Black Panther' ]
+
+// refactor with .filter()
+// .filter(): allows us to create a subset or new array with the values that satisfy a given condition
+// truthy: a true Boolean value or a condition that would equate to a true Boolean value
+  // example: true, non-empty strings, numbers except 0
+  // Boolean(): converts a value to a Boolean value
+  // console.log(Boolean(true)) //output: true
+  // console.log(Boolean("not empty")) //output: true
+  // console.log("not empty".length < 20) //output: true
+  // console.log(4 === 2 + 2) //output: true
+// falsy: a false Boolean value or a condition that would equate to a false Boolean value
+  // example: false, 0, "", null, undefined
+  // console.log(Boolean(false)) //output: false
+  // console.log(Boolean("")) //output: false
+  // console.log("not empty".length > 20) //output: false
+  // console.log(4 === "2" + 2) //output: false
+
+  // type coercion: 
+  // console.log(4 + 2) //output: 6 --> mathematical calculation performed because both are numbers, result in a number of the sum of each number
+  // console.log("4" + 2) //output: "42" --> type coercion of number 2 into a string to allow concatenation with the string of 2
 
 // test variable
 const superHero = [ "Superman", "Batman", "Wonder Woman", "Captain Marvel", "Black Panther" ]
@@ -122,6 +173,7 @@ const superHero = [ "Superman", "Batman", "Wonder Woman", "Captain Marvel", "Bla
     // use filter high order function (hof) to iterate across each value in the array
     // return a subset of the array that meets the specified condition
     // since .includes method would give values that are true to the condition of "man" included in the name, use the bang operator to give values that are false to the condition 
+
 // function expression
 const noMan = (array) => {
   return array.filter((value) => {
@@ -132,3 +184,4 @@ const noMan = (array) => {
 // function call
 console.log(noMan(superHero))
 // output: [ 'Captain Marvel', 'Black Panther' ]
+
