1dbc385c1965cdd369ae59097ea36beaf9aa69ed
Sarah Proctor <sproctor950@gmail.com>
2023-09-08 11:43:36 -0700

    adds create react app notes and a new react project

---
+++ react-intro/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./components/Header"
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>Hello Foxtrot!</p>
+      <Header />
+    </>
+  )
+}
+
+export default App
