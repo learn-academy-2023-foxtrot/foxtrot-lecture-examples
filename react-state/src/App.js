f8dccbfb0b5e41bf5e5e3b073af2c83b3204a333
Nicole <nicole@learnacademy.org>
2023-09-11 14:14:15 -0700

    React state lecture notes updated

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -1,13 +1,20 @@
 import React from "react"
+// importing our Tracker component file from components folder
 import Tracker from "./components/Tracker"
+// importing our CSS from our CSS file
 import "./App.css"
 
+// we are working in functional components (our function)
 const App = () => {
-
+  // every function needs a return of ONE thing
   return (
+    // React Fragments allow us to pass one fragment with endless nested items inside
     <>
+      {/* header for my app */}
       <h2 className="header"> Foxtrot Running Club </h2>
+      {/* header for each month / tracker */}
       <h3> January: </h3>
+      {/* component call */}
       <Tracker />
       <h3> February: </h3>
       <Tracker />
--- react-state/src/components/Tracker.js
+++ react-state/src/components/Tracker.js
@@ -1,10 +1,16 @@
+// importing our useState hook from React 
 import React, { useState } from "react"
 
 const Tracker = () => {
+  // our State variable declaration of miles with our set method that allows us to update our miles
+  // instantiating our initial value or our 
   const [miles, setMiles] = useState(0)
+  // our state variable of ocean holding the value of the color that I want to use in my inline styling later
   const [ocean, setOcean] = useState("blue")
 
+  // addMile function will allow me to update my state variable miles
   const addMile = () => {
+    // updates my miles state
     setMiles(miles + 1)
   }
   return (
