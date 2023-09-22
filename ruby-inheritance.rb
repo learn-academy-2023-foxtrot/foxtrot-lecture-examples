9d0eac81771902b746d1e7ca7067166d90c4ff89
SunkissedQueen <baxtercm@outlook.com>
2023-09-22 14:19:36 -0700

    lecture notes with additional notes and icebox answer

--- ruby-inheritance.rb
+++ ruby-inheritance.rb
@@ -14,7 +14,7 @@
   # Inheritance: pass properties and behavior to other classes
 # superclass: main class that is the template for the nested or subclasses
 # subclass: inherits properties and behavior from the superclass
-# super: 
+# super: a method to pass the values of the instance variables from the superclass to the subclass
 
 # Create a superclass NinjaTurtle which will initialize objects with a name and weapon
 
@@ -34,7 +34,7 @@ class NinjaTurtle
   end
 end
 
-# instantiation the class to create an object
+# instantiation of the super class to create an object
 # leonardo = NinjaTurtle.new('Leonardo', 'katana')
 # p leonardo
 # Output: #<NinjaTurtle:0x000000010799e230 @name="Leonardo", @weapon="katana">
@@ -48,7 +48,8 @@ end
 # class SubClass < Superclass
 # end
 
-# inheritance of the name and weapon from NinjaTurtle
+# inheritance of the values and behavior from the superclass to the subclass
+# inheritance of the name and weapon instance variables from NinjaTurtle
 # class Lenard < NinjaTurtle
 #   def initialize(name, weapon)
 #     super(name, weapon)
@@ -113,8 +114,98 @@ class Sparkle < NinjaTurtle
 end
 
 unicorn = Sparkle.new('Peter', 'rainbow spear', 'all of them')
-unicorn.defense_style
-unicorn.salutation
+# unicorn.defense_style
+# unicorn.salutation
 
-# Icebox
+
+# ****Additional Notes****
+# attr_accessor: create getter and setter methods
+
+# Superclass
+class Turtle
+  attr_accessor(:name, :weapon)
+  def initialize(name, weapon)
+    @name = name
+    @weapon = weapon
+  end
+
+  def greeter
+    p "Cowbunga, I'm #{@name}!"
+  end
+
+  def defense_style
+    p "#{@name} defends with the #{@weapon}."
+  end
+end
+
+class Pony < Turtle
+  attr_accessor(:color, :type)
+  def initialize(name, weapon, color)
+    super(name, weapon)
+    @color = color
+    @type = 'unicorn'
+  end
+
+  def salutation
+    p "HAIII!!! I'm #{@name}"
+  end
+end
+
+pony = Pony.new('Beatrice', 'cast iron skillet', 'teal')
+# pony.defense_style
+# # Output: "Beatrice defends with the cast iron skillet."
+# pony.salutation
+# Output: "HAIII!!! I'm Beatrice"
+
+# getter method from attr_accessor
+# p pony.name
+# # Output: "Beatrice"
+# p pony.weapon
+# # Output: "cast iron skillet"
+# p pony.color
+# # Output: "teal"
+# p pony.type
+# # Output: "unicorn"
+
+# setter method from attr_accessor
+# p pony
+# # output: #<Pony:0x0000000100b13360 @name="Beatrice", @weapon="cast iron skillet", @color="teal", @type="unicorn">
+
+# reassign new values for the instance variables
+pony.name = 'Beatrix'
+pony.weapon = 'run-on sentences'
+# p pony
+# # output: #<Pony:0x0000000102f33088 @name="Beatrix", @weapon="run-on sentences", @color="teal", @type="unicorn"> 
+
+# ICEBOX 🥶
 # Can subclasses inherit from other subclasses?
+
+# Yes, it is a bit tricky. Here is an example
+# this subclass will inherit from the subclass Pony
+# because the Pony class requires three arguments, we can assign those values on the super method
+class DanceBattle < Pony
+  # attr_accessor(:opponent)
+  def initialize(opponent)
+    super('Penelope', 'bad cooking', 'trashcan water')
+    @opponent = opponent
+  end
+
+  def challenge
+    p "#{@name} is challenging #{@opponent} to a dance off."
+  end
+end
+
+# when instantiating the DanceBattle it will require three arguments because it inherited from the subclass Pony which requires three arguments
+dancer = DanceBattle.new('Lenard')
+# p dancer
+# Output: #<DanceBattle:0x0000000102602900 @name="Penelope", @weapon="bad cooking", @color="trashcan water", @type="unicorn", @opponent="Lenard">
+dancer.challenge
+# Output: "Penelope is challenging Lenard to a dance off."
+
+# Here is the power of inheritance
+dancer.salutation
+# Output: "HAIII!!! I'm Penelope"
+dancer.greeter
+# Output: "Cowbunga, I'm Penelope!"
+dancer.defense_style
+# Output: "Penelope defends with the bad cooking.
